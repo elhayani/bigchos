263bd73ac1d29d0a7319e28a6b909d9ebac1b0cb
el hayani <zelhayani@gmail.com>
2019-11-10 10:04:43 +0100

    google

--- src/Controller/ConnexionController.php
+++ src/Controller/ConnexionController.php
@@ -2,28 +2,92 @@
 
 namespace App\Controller;
 
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConnexionController extends AbstractController
 {
     /**
-     * @Route("/connexion", name="connexion")
+     * Link to this controller to start the "connect" process
+     * @param ClientRegistry $clientRegistry
+     *
+     * @Route("/connect/facebook", name="connect_facebook_start")
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
      */
-    public function index()
+    public function connectFacebookAction(ClientRegistry $clientRegistry)
     {
-        return $this->render('connexion/index.html.twig', [
-            'controller_name' => 'ConnexionController',
-        ]);
+        return $clientRegistry
+            ->getClient('facebook')
+            ->redirect([
+                'public_profile', 'email' // the scopes you want to access
+            ])
+            ;
     }
 
     /**
-     * @Route("/connfacebook", name="connfacebook")
+     * After going to Facebook, you're redirected back here
+     * because this is the "redirect_route" you configured
+     * in config/packages/knpu_oauth2_client.yaml
+     *
+     * @param Request $request
+     * @param ClientRegistry $clientRegistry
+     *
+     * @Route("/connect/facebook/check", name="connect_facebook_check")
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
      */
-    public function connfacebook()
+    public function connectCheckAction(Request $request, ClientRegistry $clientRegistry)
     {
-        return $this->render('connexion/index.html.twig', [
-            'controller_name' => 'ConnexionController',
-        ]);
+        return $this->redirectToRoute('connfacebook');
+    }
+
+
+    /**
+     * Link to this controller to start the "connect" process
+     *
+     * @Route("/connect/google", name="connect_google")
+     * @param ClientRegistry $clientRegistry
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function connectGoogleAction(ClientRegistry $clientRegistry)
+    {
+        return $clientRegistry
+            ->getClient('google')
+            ->redirect();
+    }
+
+    /**
+     * social redirects to back here afterwards
+     *
+     * @Route("/connect/facebook/check", name="connect_facebook_check")
+     * @param Request $request
+     * @return JsonResponse|\Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function connectFacebookCheckAction(Request $request)
+    {
+        if (!$this->getUser()) {
+            return new JsonResponse(array('status' => false, 'message' => "User not found!"));
+        } else {
+            return $this->redirectToRoute('default');
+        }
+
+        /**
+         * social redirects to back here afterwards
+         *
+         * @Route("/connect/google/check", name="connect_google_check")
+         * @param Request $request
+         * @return JsonResponse|\Symfony\Component\HttpFoundation\RedirectResponse
+         */
+        public function connectGoogleCheckAction(Request $request)
+    {
+        if (!$this->getUser()) {
+            return new JsonResponse(array('status' => false, 'message' => "User not found!"));
+        } else {
+            return $this->redirectToRoute('default');
+        }
+
     }
 }
